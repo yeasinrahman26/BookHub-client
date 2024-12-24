719f32c4fb97df80a0860d3047d9911f258d290c
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2024-12-25 05:45:15 +0600

    adding footer

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -2,9 +2,35 @@
 
 const Footer = () => {
     return (
-        <div>
-            <h2>I am footer</h2>
-        </div>
+      <footer className="footer bg-base-300 mt-5 text-base-content p-10">
+        <aside>
+          <p className="text-2xl font-semibold">
+            BookHub Ltd
+
+          </p>
+          <p> Providing reliable books since 2013</p>
+        </aside>
+        <nav>
+          <h6 className="footer-title">Library Services</h6>
+          <a className="link link-hover">Catalog Search</a>
+          <a className="link link-hover">Online Resources</a>
+          <a className="link link-hover">Membership</a>
+          <a className="link link-hover">Events</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">About Us</h6>
+          <a className="link link-hover">Our Story</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Careers</a>
+          <a className="link link-hover">Press</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of Use</a>
+          <a className="link link-hover">Privacy Policy</a>
+          <a className="link link-hover">Cookie Policy</a>
+        </nav>
+      </footer>
     );
 };
 
