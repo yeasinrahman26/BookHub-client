d671c1ee39f74b07897d485b3683dd1791323111
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2024-12-26 04:29:34 +0600

    all book done with card

---
+++ src/Pages/AllBook/AllBook.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import AllBookCard from "./AllBookCard";
+
+
+const AllBook = () => {
+
+  const [books,setBooks]=useState([])
+  useEffect(()=>{
+    fetch("http://localhost:5000/books")
+    .then(res=>res.json())
+    .then(data=>{
+      setBooks(data);
+    })
+  },[])
+
+
+
+    return (
+      <div>
+        <Helmet>
+          <title>BookHub || All Book</title>
+        </Helmet>
+        <h1 className="text-5xl text-center my-10 ">
+          I am All books: {books.length}{" "}
+        </h1>
+        <div className="grid gap-10 px-2 grid-cols-1 md:grid-cols-2 mb-10 lg:grid-cols-3 ">
+          {books.map((book) => (
+            <AllBookCard key={book._id} book={book}></AllBookCard>
+          ))}
+        </div>
+      </div>
+    );
+};
+
+export default AllBook;
---
+++ src/Pages/AllBook/AllBookCard.jsx
@@ -0,0 +1,57 @@
+
+
+const AllBookCard = ({book}) => {
+    const {
+      _id,
+      title,
+      image,
+      quantity,
+      author_name,
+      category,
+      short_description,
+      rating,
+      book_content,
+    } = book;
+    const truncateDescription = (description, wordLimit) => {
+      const words = description.split(" ");
+      return words.length > wordLimit
+        ? words.slice(0, wordLimit).join(" ") + "..."
+        : description;
+    };
+    return (
+      <div className="card bg-base-100 border-2 shadow-xl p-4 rounded-lg">
+        <figure>
+          <img
+            src={image}
+            alt={title}
+            className="h-48 border p-1 border-black  object-cover rounded-xl"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-2xl font-bold ">{title}</h2>
+          <p className="text-gray-700 ">
+            <strong>Author:</strong> {author_name}
+          </p>
+          <p className="text-gray-700 ">
+            <strong>Category:</strong> {category}
+          </p>
+          <p className="text-gray-700 ">
+            <strong>Rating:</strong> {rating} / 5
+          </p>
+          <p className="text-gray-700 ">
+            <strong>Quantity Available:</strong> {quantity}
+          </p>
+          <p className="text-gray-700 ">
+            <strong>Short Description:</strong>{" "}
+            {truncateDescription(short_description, 5)}
+          </p>
+          <p className="text-gray-700 ">
+            <strong>Book Content:</strong> {truncateDescription(book_content,10)}
+          </p>
+          <button className="btn btn-primary ">View Details</button>
+        </div>
+      </div>
+    );
+};
+
+export default AllBookCard;
